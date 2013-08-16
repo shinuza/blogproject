5015abfa5adb5667a152e185f7f9779c05223d37
Samori Gorse <samorigorse@gmail.com>
2013-08-16 18:49:10 +0200

    Document creation date support

--- test/document.js
+++ test/document.js
@@ -11,6 +11,10 @@ function plaindoc() {
   return new Document(path.resolve(__dirname, './sandbox/documents/plain-document.md'));
 }
 
+function type(obj) {
+  return Object.prototype.toString.call(obj).match(/\[object (\w+)\]/)[1];
+}
+
 
 suite('Meta');
 
@@ -22,9 +26,17 @@ test('title', function() {
   assert.equal(metadoc().title(), 'A different title');
 });
 
+test('created date', function() {
+  assert.equal(metadoc().ctime(), '1 January, 2013');
+});
+
 
 suite('Plain');
 
 test('title', function() {
   assert.equal(plaindoc().title(), 'A plain document title');
+});
+
+test('title', function() {
+  assert.equal(type(plaindoc().ctime()), 'Date');
 });
--- test/sandbox/documents/meta-document.md
+++ test/sandbox/documents/meta-document.md
@@ -1,6 +1,6 @@
 Title:      A different title
 Author:     John Doe, Jane Doe  
-Date:       January 1st, 2012  
+Date:       1 January, 2013
 Keywords:   blog, test, anything, goes  
 
 A title
